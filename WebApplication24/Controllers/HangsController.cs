6477b96730fcac9dd393efcfabf5865818532a7a
ADMIN <nguyendong.g6land@gmail.com>
2022-12-21 23:22:39 +0700

    day la bai 10

---
+++ WebApplication24/Controllers/HangsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication24.Models;
+using PagedList;
+
+namespace WebApplication24.Controllers
+{
+    public class HangsController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: Hangs
+        public ActionResult Index(string sortOrder, string searchString, string currentFilter , int? page)
+        {
+            // cac bien sap xep 
+            ViewBag.CurrentSort=sortOrder;
+            ViewBag.SapTheoTen = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.SapTheoGia = sortOrder == "Gia" ? "gia_desc" : "Gia";
+            if (searchString != null)
+            {
+                page = 1;// trang dau tien 
+            }
+            // lay danh sach hang
+
+            var hangs = db.Hangs.Select(p => p);
+
+            // loc theo ten hang
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                hangs = hangs.Where(p => p.TenHang.Contains(searchString));
+            }
+            // sap xep 
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    hangs = hangs.OrderByDescending(s => s.TenHang);
+                    break;
+                case "Gia":
+                    hangs = hangs.OrderBy(s => s.Gia);
+                    break;
+                case "gia_desc":
+                    hangs = hangs.OrderByDescending(s => s.Gia);
+                    break;
+                default:  
+                    hangs=hangs.OrderBy(s => s.TenHang);
+                    break;
+            }
+
+            int PageSize = 3;
+
+            int pageNumber = (page ?? 1);
+
+            return View(hangs.ToPagedList(pageNumber , PageSize));
+        }
+
+        // GET: Hangs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hang hang = db.Hangs.Find(id);
+            if (hang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hang);
+        }
+
+        // GET: Hangs/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaNCC = new SelectList(db.Nha_CC, "MaNCC", "TenNCC");
+            return View();
+        }
+
+        // POST: Hangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaHang,MaNCC,TenHang,Gia,LuongCo,MoTa,ChietKhau,HinhAnh")] Hang hang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Hangs.Add(hang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaNCC = new SelectList(db.Nha_CC, "MaNCC", "TenNCC", hang.MaNCC);
+            return View(hang);
+        }
+
+        // GET: Hangs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hang hang = db.Hangs.Find(id);
+            if (hang == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaNCC = new SelectList(db.Nha_CC, "MaNCC", "TenNCC", hang.MaNCC);
+            return View(hang);
+        }
+
+        // POST: Hangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaHang,MaNCC,TenHang,Gia,LuongCo,MoTa,ChietKhau,HinhAnh")] Hang hang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaNCC = new SelectList(db.Nha_CC, "MaNCC", "TenNCC", hang.MaNCC);
+            return View(hang);
+        }
+
+        // GET: Hangs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hang hang = db.Hangs.Find(id);
+            if (hang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hang);
+        }
+
+        // POST: Hangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Hang hang = db.Hangs.Find(id);
+            db.Hangs.Remove(hang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication24/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication24.Models;
+using PagedList;
+
+namespace WebApplication24.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Model1 db = new Model1(); 
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult displaySuplier(int? page)
+        {
+            var suppliers = db.Nha_CC.Select(s => s);
+            // sap xep truoc khi phan trang
+            suppliers = suppliers.OrderBy(s => s.MaNCC);
+            int pageSize = 3;
+            int pageNumber=(page ??1);
+            return View(suppliers.ToPagedList(pageNumber,pageSize));
+        }
+    }
+}
